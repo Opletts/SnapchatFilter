1ad62f1a47acd69be3854b98799e48aa9b9f2551
Apratim Mukherjee <apratim101@gmail.com>
2017-07-18 23:18:59 +0530

    Lens overlay

--- Haar.py
+++ Haar.py
@@ -10,16 +10,12 @@ while(True):
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 	face_cascade = cv2.CascadeClassifier('/opt/opencv-3.1.0/data/haarcascades/haarcascade_frontalface_default.xml')
-	eye_cascade = cv2.CascadeClassifier('/opt/opencv-3.1.0/data/haarcascades/haarcascade_eye.xml')
-
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
 	for (x,y,w,h) in faces:
 		ch = cv2.imread('Cherizerd.png') #read the image you want to overlay on the face
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-		roi_gray = gray[y:y+h, x:x+w]
-		roi_color = img[y:y+h, x:x+w]
-		ch = cv2.resize(ch, (h,w))
+		ch = cv2.resize(ch, (w,h))
 
 		roi = img[y:y+h, x:x+w]
 
@@ -31,9 +27,5 @@ while(True):
 		dst = cv2.add(bg, fg)
 		img[y:y+h, x:x+w] = dst 
 
-		#eyes = eye_cascade.detectMultiScale(roi_gray)
-		#for (ex,ey,ew,eh) in eyes:
-		#	cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	#cv2.imshow('ch', ch)
 	cv2.imshow('img',img)	
 	cv2.waitKey(1)
--- lens.py
+++ lens.py
@@ -12,7 +12,31 @@ while(True):
 	eye_cascade = cv2.CascadeClassifier('/opt/opencv-3.1.0/data/haarcascades/haarcascade_eye.xml')
 
 	eyes = eye_cascade.detectMultiScale(gray)
-	for (ex,ey,ew,eh) in eyes:
-		cv2.rectangle(img,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-	cv2.imshow("Eyes", img)
-	cv2.waitKey(1)
+	
+	if len(eyes) == 2:
+
+		x = min(eyes[0][0], eyes[1][0])
+		if x == eyes[0][0]:
+			w = eyes[1][0]+eyes[1][2]-eyes[0][0]
+		else:
+			w = eyes[0][0]+eyes[0][2]-eyes[1][0]
+
+		y = min(eyes[0][1], eyes[1][1])
+		h = max(eyes[0][3], eyes[1][3])
+
+		gl = cv2.imread('/home/opletts/Stuff/glass.jpg')
+		#cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
+		gl = cv2.resize(gl, (w+50,h+50))
+
+		roi = img[y-25:y+h+25, x-25:x+w+25]
+
+		glgray = cv2.cvtColor(gl,cv2.COLOR_BGR2GRAY)
+		_, mask = cv2.threshold(glgray, 10, 255, cv2.THRESH_BINARY)
+		mask_inv = cv2.bitwise_not(mask)
+		bg = cv2.bitwise_and(roi,roi,mask = mask_inv)
+		fg = cv2.bitwise_and(gl,gl,mask = mask)
+		dst = cv2.add(bg, fg)
+		img[y-25:y+h+25, x-25:x+w+25] = dst
+
+		cv2.imshow("Eyes", img)
+		cv2.waitKey(1)
